c3bae588164c6369c511c2b5077a55b5726de8f4
Joe9238 <joemaskell@outlook.com>
2019-09-09 18:44:20 +0100

    Attempt at solution

--- Logic.py
+++ Logic.py
@@ -10,36 +10,57 @@ def main():
     hnum = int(input("Enter the number of hydrogen atoms: "))
     strcnum = str(cnum)
     strhnum = str(hnum)
-    list1 = [0, 0, 0, 0, 0]
-    list2 = [0, 0, 0, 0, 0]
-    list3 = [0, 0, 0, 0, 0]
-    list4 = [0, 0, 0, 0, 0]
-    list5 = [0, 0, 0, 0, 0]
-
-    global alkane
-    global alkene
-    global cyclic
-
-    alkane = False
-    alkene = False
-    cyclic = False
+    global array
+    array = [
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+         " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+         " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+         " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+         " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+         " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+         " ", " ", " "]]
 
     straightchain()
 
 
 def straightchain():
-    if hnum == (cnum * 2) + 2:
-        alkane = True
-        alkane()
-    elif hnum == cnum * 2:
-        alkene = True
-        alkene()
-    elif hnum == cnum and cnum >= 6:
-        cyclic = True
-        cyclic()
-    else:
-        print("Error - Unknown Hydrocarbon")
-        main()
+    fillarray()
+    for row in array:
+        for elem in row:
+            print(elem, end=' ')
+        print()
+
+        if hnum == (cnum * 2) + 2:
+            alkane()
+        elif hnum == cnum * 2:
+            alkene()
+        elif hnum == cnum and cnum >= 6:
+            cyclic()
+        else:
+            print("Error - Unknown Hydrocarbon")
+            main()
+
+
+def fillarray():  # DOWN AND THEN ALONG - STARTS AT [0][0]
+    n = 1
+    i = 2 * n
+    array[i][i] = "C"
+    array[i + 1][i] = "|"
+    array[i - 1][i] = "|"
+    array[i][i + 1] = "-"
+    array[i][i - 1] = "-"
+    if cnum > 1:
+        for x in range(cnum - 1):
+            array[i][(x + 2) * i] = "C"
+            array[i + 1][i * (x + 2)] = "|"
+            array[i - 1][i * (x + 2)] = "|"
+            array[i][(i * (x + 2)) + 1] = "-"
+            array[i][(i * (x + 2)) - 1] = "-"
 
 
 def alkane():
@@ -53,3 +74,5 @@ def alkene():
 def cyclic():
     pass
 
+
+main()
