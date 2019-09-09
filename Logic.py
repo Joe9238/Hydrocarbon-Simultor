2f0433b62388fbbabd4d199fbb8aa3d1a45a4fd0
Joe9238 <joemaskell@outlook.com>
2019-09-09 18:48:16 +0100

    Attempt at solution

--- Logic.py
+++ Logic.py
@@ -1,74 +1,110 @@
 def main():
     global cnum
     global hnum
-    global list1
-    global list2
-    global list3
-    global list4
-    global list5
+    global strcnum
+    global strhnum
+    global SUB
+    global SUBimp
+    global SUP
+    SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+    SUBimp = str.maketrans("(2n+2)-", "₍₂ₙ₊₂₎₋")
+    SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
     cnum = int(input("Enter the number of carbon atoms: "))
     hnum = int(input("Enter the number of hydrogen atoms: "))
     strcnum = str(cnum)
     strhnum = str(hnum)
     global array
-    array = [
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-         " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-         " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-         " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-         " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-         " ", " ", " "],
-        [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
-         " ", " ", " "]]
+    array = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
     straightchain()
 
 
 def straightchain():
-    fillarray()
-    for row in array:
-        for elem in row:
-            print(elem, end=' ')
-        print()
-
-        if hnum == (cnum * 2) + 2:
-            alkane()
-        elif hnum == cnum * 2:
-            alkene()
-        elif hnum == cnum and cnum >= 6:
-            cyclic()
-        else:
-            print("Error - Unknown Hydrocarbon")
-            main()
+    if hnum == (cnum * 2) + 2:
+        alkane()
+    elif hnum == cnum * 2:
+        alkene()
+    elif hnum == cnum and cnum >= 6:
+        cyclic()
+    else:
+        print("Error - Unknown Hydrocarbon")
+        main()
 
 
-def fillarray():  # DOWN AND THEN ALONG - STARTS AT [0][0]
+def alkane():  # DOWN AND THEN ALONG - STARTS AT [0][0]
     n = 1
     i = 2 * n
-    array[i][i] = "C"
+    array[i][i] = n
     array[i + 1][i] = "|"
     array[i - 1][i] = "|"
     array[i][i + 1] = "-"
     array[i][i - 1] = "-"
     if cnum > 1:
         for x in range(cnum - 1):
-            array[i][(x + 2) * i] = "C"
+            array[i][(x + 2) * i] = x + 2
             array[i + 1][i * (x + 2)] = "|"
             array[i - 1][i * (x + 2)] = "|"
             array[i][(i * (x + 2)) + 1] = "-"
             array[i][(i * (x + 2)) - 1] = "-"
+    for x in range(cnum):
+        array[i + 2][(2 * x) + 2] = "H"
+        array[i - 2][(2 * x) + 2] = "H"
+    array[i][i - 2] = "H"
+    array[i][(i * cnum) + 2] = "H"
 
+    for row in array:
+        for elem in row:
+            print(elem, end=' ')
+        print()
 
-def alkane():
-    pass
+    print("""
+  """)
+    molformula = str("C" + strcnum + "H" + strhnum)
+    print("The molecular formula of the hydrocarbon is " + molformula.translate(SUB))
+    print("The hydrocarbon is an alkane")
 
 
 def alkene():
-    pass
+    n = 1
+    i = 2 * n
+    array[i][i] = n
+    array[i + 1][i] = "|"
+    array[i - 1][i] = "|"
+    array[i][i + 1] = "-"
+    array[i][i - 1] = "-"
+    if cnum > 1:
+        for x in range(cnum - 1):
+            array[i][(x + 2) * i] = x + 2
+            array[i + 1][i * (x + 2)] = "|"
+            array[i - 1][i * (x + 2)] = "|"
+            array[i][(i * (x + 2)) + 1] = "-"
+            array[i][(i * (x + 2)) - 1] = "-"
+    for x in range(cnum):
+        array[i + 2][(2 * x) + 2] = "H"
+        array[i - 2][(2 * x) + 2] = "H"
+    array[i][i - 2] = "H"
+    array[i][(i * cnum) + 2] = "H"
+
+    for row in array:
+        for elem in row:
+            print(elem, end=' ')
+        print()
+
+    print("""
+  """)
+    molformula = str("C" + strcnum + "H" + strhnum)
+    print("The molecular formula of the hydrocarbon is " + molformula.translate(SUB))
+    print("The hydrocarbon is an alkene")
 
 
 def cyclic():
